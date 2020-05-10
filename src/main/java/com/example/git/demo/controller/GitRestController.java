1eb3e3d13103905d5291188db21db6e32c443fad
sureshpec04 <kalpanahome@kalpanas-mbp.hsd1.il.comcast.net>
2020-05-10 16:51:36 -0500

    added employee by id endpoint

--- src/main/java/com/example/git/demo/controller/GitRestController.java
+++ src/main/java/com/example/git/demo/controller/GitRestController.java
@@ -1,6 +1,7 @@
 package com.example.git.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,12 @@ public class GitRestController {
 	@GetMapping
 	public String sayHello() {
 		return "Hello GitHub from Eclipse";
+	}
+	
+	@GetMapping("/employee/{id")
+	public String findEmployeeById(@PathVariable(value = "id") final String id) {
+		
+		return "Found employee by id: " + id;
 	}
 
 }
