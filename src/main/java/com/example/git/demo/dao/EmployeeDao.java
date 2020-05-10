1474f79b47949d2dc6008caad80596a522fca406
sureshpec04 <kalpanahome@kalpanas-mbp.hsd1.il.comcast.net>
2020-05-10 16:59:37 -0500

    Added dao for prod release 2

---
+++ src/main/java/com/example/git/demo/dao/EmployeeDao.java
@@ -0,0 +1,9 @@
+package com.example.git.demo.dao;
+
+public class EmployeeDao {
+
+	
+	public void insertEmployee(String id, String name) {
+		System.out.println("Employee inserted with id: " + id + " name: " + name);
+	}
+}
