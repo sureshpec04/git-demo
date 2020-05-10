d5939d8e782745d1e66b09b04497c6822b0b562c
sureshpec04 <kalpanahome@kalpanas-mbp.hsd1.il.comcast.net>
2020-05-10 16:18:46 -0500

    Renamed test files

--- src/main/java/com/example/git/demo/GitDemoApp.java
+++ src/main/java/com/example/git/demo/GitDemoApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GitDemo1Application {
+public class GitDemoApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GitDemo1Application.class, args);
+		SpringApplication.run(GitDemoApp.class, args);
 	}
 
 }
--- src/test/java/com/example/git/demo/GitDemoAppTests.java
+++ src/test/java/com/example/git/demo/GitDemoAppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GitDemo1ApplicationTests {
+class GitDemoAppTests {
 
 	@Test
 	void contextLoads() {
